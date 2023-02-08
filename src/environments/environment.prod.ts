04a5ad7f02025c7a4c5bd8e64b10916e67ca625f
Ubuntu <ubuntu@ip-172-31-30-171.us-east-2.compute.internal>
2023-02-08 11:31:05 +0000

    Claims-UI

---
+++ src/environments/environment.prod.ts
@@ -0,0 +1,6 @@
+export const environment = {
+  production: true,
+   CLAIMS_URL:'http://20.102.105.189:9000',
+   FACILITY_URL:'http://20.102.105.189:9001',
+   CUSTOMER_URL:'http://20.102.105.189:9002'
+};
